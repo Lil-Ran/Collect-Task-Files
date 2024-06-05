447e3c3b3fc6976d8454463aa586579384109e88
Lil-Ran <lilran4869@qq.com>
2024-06-05 21:08:04 +0800

    wip: update default params, prompts, and comments; rename variables

--- gzctf_attachment_downloader.py
+++ gzctf_attachment_downloader.py
@@ -7,6 +7,7 @@ import sys
 
 def main():
     args = arg_parse()
+    # print(args)
     get_challs(args)
 
 def get_challs(args):
@@ -15,7 +16,7 @@ def get_challs(args):
     # get game title
     response = requests.get(args.url, headers=headers)
     if response.status_code != 200:
-        print('❌', f'Failed to get game info from {args.url}, status code: {response.status_code}')
+        print('❌', f'Failed to get game title from {args.url}, status code: {response.status_code}')
         sys.exit(1)
 
     game_info = response.json()
@@ -39,28 +40,30 @@ def get_challs(args):
                 print('❌', f'Failed to get challenge {object["id"]}, error: {e}')
                 # traceback.print_exc()
 
+    print('🎉', 'All done.')
+
 def get_one_chall(args, id: int, headers: dict, game_title: str):
 
     # get attachment info, including URL
     
-    url = f'{args.url}/challenges/{id}'
-    response = requests.get(url, headers=headers)
+    url_chall_id = f'{args.url}/challenges/{id}'
+    response = requests.get(url_chall_id, headers=headers)
     if response.status_code != 200:
-        print('❌', f'Failed to get challenge info from {url}, status code: {response.status_code}')
+        print('❌', f'Failed to get challenge info from {url_chall_id}, status code: {response.status_code}')
         return
 
     response_json = response.json()
     name = response_json['title']
     tag = response_json['tag'].lower()
-    remote_path = response_json['context']['url']    # may be relative or absolute
-    size = response_json['context']['fileSize']      # may be null
+    remote_path = response_json['context']['url']         # may be relative or absolute
+    info_size = response_json['context']['fileSize']      # may be null
     
     if remote_path is None:
         print('⏩', f'{tag}/{name}'.ljust(24), 'has no attachment')
         return
     
-    if size is not None and size > args.max_size:
-        print('🤯', f'{tag}/{name}'.ljust(24), f'is too large ({format(size, ",")} bytes)')
+    if info_size is not None and info_size > args.max_size:
+        print('🤯', f'{tag}/{name}'.ljust(24), f'is too large ({format(info_size, ",")} bytes)')
         return
     
     # get attachment file name and size
@@ -79,13 +82,14 @@ def get_one_chall(args, id: int, headers: dict, game_title: str):
     
     if response.headers.get('Content-Type', default='').split('/')[0] == 'text':
         print('❔', f'{tag}/{name}'.ljust(24), f'Content-Type: {response.headers.get("Content-Type", default="")}')
+        # not return
 
     raw_size = int(response.headers.get('Content-Range', default='0-0/-1').split('/')[-1])
     if raw_size != -1 and raw_size > args.max_size:
         print('🤯', f'{tag}/{name}'.ljust(24), f'is too large ({format(raw_size, ",")} bytes)')
         return
     
-    size = raw_size if size is None else max(size, raw_size)
+    size = raw_size if info_size is None else max(info_size, raw_size)
 
     raw_file_name = response.headers.get('Content-Disposition', default='filename=NONE') \
                                     .split('filename=')[1] \
@@ -133,7 +137,7 @@ def get_one_chall(args, id: int, headers: dict, game_title: str):
             got_size += len(chunk)
             print('\r📥',
                   f'{tag}/{name}'.ljust(24),
-                  '>' * min(got_size*50//size, 50) + '_' * min(50-got_size*50//size, 50),
+                  '>' * min(got_size*40//size, 40) + '_' * min(40-got_size*40//size, 40),
                   f'{got_size}/{size} bytes',
                   end='')
 
@@ -147,31 +151,31 @@ def arg_parse():
     parser = argparse.ArgumentParser(description='A GZ::CTF attachment downloader.')
     parser.add_argument('-u', '--url', type=str, help='GZ::CTF game URL, e.g. https://example.com/games/1/challenges or https://example.com/games/1')
     parser.add_argument('-t', '--token', type=str, help='value of cookie GZCTF_TOKEN')
-    parser.add_argument('-d', '--root-directory', type=str, default='{game}', help='root directory, default is `pwd`/{game}, e.g. ./LRCTF 2024')
-    parser.add_argument('-f', '--file-path', type=str, default='{tag}/{chall}/{chall}_{raw}', help='style of file path, default is {tag}/{chall}/{chall}_{raw}, e.g. misc/sign in/sign in_attachment_deadbeef.zip')
+    parser.add_argument('-d', '--root-directory', type=str, default='{game}', help='default is `pwd`/{game}, which generates "./LRCTF 2024"')
+    parser.add_argument('-f', '--file-path', type=str, default='{tag}/{chall}/{raw}', help='style of file path, default is {tag}/{chall}/{raw}, which generates "misc/sign in/attachment_deadbeef.zip"')
     
     # {game}    received game title, e.g. "LRCTF 2024"
-    # {tag}     direction in lowercase, e.g. "misc"
+    # {tag}     "direction" in lowercase, e.g. "misc"
     # {chall}   received challenge name, e.g. "sign in"
     # {raw}     received file name, e.g. "attachment_deadbeef.zip"
 
     parser.add_argument('-k', '--keep-spaces', action="store_true", help='''if specified, "--file-path" with spaces will not be replaced by '-' ''')
-    parser.add_argument('-s', '--max-size', type=float, default=10.0, help='max file size in MB, larger than this will be skipped, default is 10.0, set to 0 to disable')
+    parser.add_argument('-s', '--max-size', type=float, default=50.0, help='max file size in MB, larger than this will be skipped, default is 50.0, set to 0 to disable')
     parser.add_argument('-o', '--overwrite', action="store_true", help='if specified, existing files will be replaced instead of skipped')
     
-    direction_group = parser.add_argument_group('direction options, default is ALL, you can specify like -mwp')
-    direction_group.add_argument('-E', '--except-mode', action="store_true", help='-w means ONLY download web, while -E -w means download everything else EXCEPT web')
-    direction_group.add_argument('-m', '--misc', action='store_true')
-    direction_group.add_argument('-c', '--crypto', action='store_true')
-    direction_group.add_argument('-p', '--pwn', action='store_true')
-    direction_group.add_argument('-w', '--web', action='store_true')
-    direction_group.add_argument('-r', '--reverse', action='store_true')
-    direction_group.add_argument('--blockchain', action='store_true')
-    direction_group.add_argument('--forensics', action='store_true')
-    direction_group.add_argument('--hardware', action='store_true')
-    direction_group.add_argument('--mobile', action='store_true')
-    direction_group.add_argument('--ppc', action='store_true')
-    direction_group.add_argument('--ai', action='store_true')
+    tag_group = parser.add_argument_group('tag options, default is ALL, you can specify like -mwp')
+    tag_group.add_argument('-E', '--except-mode', action="store_true", help='-w means ONLY download web, while -E -w means download everything else EXCEPT web')
+    tag_group.add_argument('-m', '--misc', action='store_true')
+    tag_group.add_argument('-c', '--crypto', action='store_true')
+    tag_group.add_argument('-p', '--pwn', action='store_true')
+    tag_group.add_argument('-w', '--web', action='store_true')
+    tag_group.add_argument('-r', '--reverse', action='store_true')
+    tag_group.add_argument('--blockchain', action='store_true')
+    tag_group.add_argument('--forensics', action='store_true')
+    tag_group.add_argument('--hardware', action='store_true')
+    tag_group.add_argument('--mobile', action='store_true')
+    tag_group.add_argument('--ppc', action='store_true')
+    tag_group.add_argument('--ai', action='store_true')
 
     args = parser.parse_args()
 
@@ -185,21 +189,21 @@ def arg_parse():
     # https://example.com/api/game/1
 
     if args.token is None:
-        args.token = input('\nEnter GZCTF_TOKEN cookie value here: ').strip()
+        args.token = input('\nPaste GZCTF_TOKEN cookie value here: ').strip()
     args.token = args.token.replace('GZCTF_TOKEN=', '').strip()
 
     args.max_size = args.max_size * 1024 * 1024 if args.max_size > 0 else float('inf')
 
-    direction_list = ['misc', 'crypto', 'pwn', 'web', 'reverse', 'blockchain', 'forensics', 'hardware', 'mobile', 'ppc', 'ai']
-    allowlist = direction_list.copy()
-    if any(getattr(args, direction) for direction in direction_list):
-        for direction in direction_list:
-            if bool(getattr(args, direction)) ^ (not args.except_mode):
-                allowlist.remove(direction)
+    tag_list = ['misc', 'crypto', 'pwn', 'web', 'reverse', 'blockchain', 'forensics', 'hardware', 'mobile', 'ppc', 'ai']
+    allowlist = tag_list.copy()
+    if any(getattr(args, tag) for tag in tag_list):
+        for tag in tag_list:
+            if bool(getattr(args, tag)) ^ (not args.except_mode):
+                allowlist.remove(tag)
     
     args.allowlist = allowlist
-    for direction in direction_list:
-        delattr(args, direction)
+    # for tag in tag_list:
+    #     delattr(args, tag)
 
     return args
 
